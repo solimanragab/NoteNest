a7510e8c735a6e29c7d3a3681957f74cab5668ef
solimanragab <ragabsoliman63@gmail.com>
2024-11-16 13:40:18 +0200

    push the whole website

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LightbulbIcon from '@mui/icons-material/Lightbulb';function Header() {
+  return (
+    <header>
+      <h1>
+        <LightbulbIcon />
+        NoteNest
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
